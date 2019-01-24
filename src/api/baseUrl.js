b903d0cc4cff5641a7a08f6f50d949c0bc3f2836
Blaine Taylor <blainert25@gmail.com>
2019-01-24 07:09:46 -0700

    Added Hosted Heroku URL to the production version of the app + Dist Folder

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://intense-shelf-99491.herokuapp.com/';
   }
 
   function getQueryStringParameterByName(name, url) {
